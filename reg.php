1fcd82b1cdb90ac66da7b02eb240f04ac44208f8
AdmirKaralic <admirkaralic@gmail.com>
2018-02-25 12:10:36 +0100

    uradjeno stajliranje registracije

--- reg.php
+++ reg.php
@@ -3,27 +3,58 @@
 <head>
     <meta charset="UTF-8">
     <title>Registracija</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
-   
+   <div class="container">
+    <hr>
+   <h1 class="text-center"> Registration </h1> </br>
+   <hr>
+
    <form action="" method="post">
+   <div class="form-row text-center">
+   <div class="form-group col-lg-6">
        <label for="user">Username</label>
-       <input type="text" name="username" id="user"> </br> </br>
+       <input  class="form-control form-control-sm" type="text" name="username" id="user"> 
+	   
+	   </div>
+	   <div class="form-group col-lg-6">
 	   <label for="pass">Password</label>
-       <input type="password" name="password" id="pass"> </br> </br>
+       <input class="form-control form-control-sm" type="password" name="password" id="pass"> 
+	   
+	   </div>
+	   </div>
+	   <div class="form-row text-center">
+	   <div class="form-group col-lg-6">
 	   <label for="ime">Ime I Prezime</label>
-       <input type="text" name="imeprezime" id="ime"> </br> </br>
+       <input class="form-control form-control-sm" type="text" name="imeprezime" id="ime">
+	   
+	   </div>
+	   <div class="form-group col-lg-6">
 	   <label for="mail">Email</label>
-       <input type="email" name="email" id="mail"> </br> </br>
-	   <label for="dob">Datum Rodzenja</label>
-       <input type="text" name="dob" id="dob"> </br> </br>
+       <input class="form-control form-control-sm" type="email" name="email" id="mail"> 
+	   </div>
+	   
+	   </div>
+	   <div class="form-row text-center">
+	        <div class="form-group col-lg-6">
+	          <label for="dob">Datum Rodzenja</label>
+              <input class="form-control form-control-sm" type="date" name="dob" id="dob">
+	        </div>
+	  
+	   <div class="form-group col-lg-6">
 	   <label for="adresa">Adresa</label>
-       <input type="text" name="adresa" id="adresa"> </br>  </br> 
-       <input type="submit" name="submit">
-       
-       
-   </form>
-    
+       <input class="form-control form-control-sm" type="text" name="adresa" id="adresa">
+	   
+	   </div>
+	  
+       </div>
+	   <div class="text-center">
+	    <button type="submit" class="btn btn-primary">Register</button>
+		</div>
+	   </form> <!-- end of form -->
+
+    </div> <!-- end of container-->
 </body>
 </html>
 
